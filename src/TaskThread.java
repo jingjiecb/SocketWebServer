ad6f04b8f53b6bd10bceab13061690275f2ffb9b
Claws <2821624044@qq.com>
2020-05-25 20:44:17 +0800

    fix: 添加注释

--- src/TaskThread.java
+++ src/TaskThread.java
@@ -37,7 +37,7 @@ public class TaskThread extends Thread {
             parser.print();
 
 
-            if (parser.getMethod() == 1) {
+            if (parser.getMethod() == 1) {//1表示这是个POST请求
                 String content = parser.getContent();
                 String[] pair = content.split("&");
                 String userName = (pair[0].split("="))[1];
@@ -45,25 +45,27 @@ public class TaskThread extends Thread {
 
                 String path=parser.getPath();
 
-                if (path.equals("/login")) {
-                    if (userDao.isMatch(userName, passwd)) {
+                if (path.equals("/login")) {//如果是来自登陆页面的post
+                    if (userDao.isMatch(userName, passwd)) {//如果用户密码正确，颁发一个cookie给浏览器
                         responser = new SetCookieResponser(outputStream, cookieDao.getNewCookie(userName));
                         responser.send();
-                    } else {
+                    } else {//如果用户密码错误，则重定向回登陆页面
                         responser = new C301Responser(outputStream, "http://localhost:8888/login.html");
                         responser.send();
                     }
-                } else if (path.equals("/register")){
+                } else if (path.equals("/register")){//如果是来自于注册页面的post，添加一个用户
                     userDao.addUser(userName,passwd);
                     responser = new C301Responser(outputStream, "http://localhost:8888/login.html");
                     responser.send();
                 }
 
-            } else {
+            } else {//如果不是POST请求即如果是GET请求
                 String rawPath = parser.getPath();
 
                 String nameCookie = parser.getCookieByKey("username");
-                System.out.println(nameCookie);
+                // System.out.println(nameCookie);
+
+                // 如果是没有cookie的非法请求，重定向回登陆页面
                 if (!cookieDao.isValid(nameCookie) &&
                         !rawPath.equals("/login.html") &&
                         !rawPath.equals("/register.html")) {
@@ -72,7 +74,6 @@ public class TaskThread extends Thread {
                 } else {
 
                     //尝试发送文件
-
                     String path = BASEPATH + rawPath;
                     responser = new MIMEResponser(outputStream, path);
                     if (!responser.send()) {//如果文件不存在，发送404应答
